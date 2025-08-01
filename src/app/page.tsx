6b52ed529c874dceaa4400a3062f5d2ca796fc0a
App Santos <app@italosantos.com>
2025-08-01 14:38:03 +0000

    errou colocou em baixo é em cima no lugar do cabelhaçho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
   return (
     <>
       <section>
-        <div className="container text-center pt-8 pb-20 md:pb-32">
+        <div className="container text-center pb-20 md:pb-32">
           
           <div className="mb-8 flex justify-center">
             <Image 
